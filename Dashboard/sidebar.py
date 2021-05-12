d65268079acdd0edf74231eba972ca270965e050
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-05-11 20:48:14 -0700

    add work setup to sidebar and adapt graphs accordinly

--- Dashboard/sidebar.py
+++ Dashboard/sidebar.py
@@ -3,7 +3,7 @@ import streamlit as st
 def setup_sidebar():
     '''sets up the sidebar elements for streamlit
     returns: selected fatigue score and designation range, 
-    if males or females were selected, selected industry type'''
+    if males or females were selected, selected industry type and selected work setup'''
 
     st.sidebar.write("Please select applicable settings.")
     fatigue_score = st.sidebar.slider('Fatigure Score', 0., 10., (1., 9.))
@@ -16,6 +16,8 @@ def setup_sidebar():
     company_options = st.sidebar.selectbox(
         'Select Industry to be displayed',
         ('Service', 'Product', 'Both'))
-    
+    wfh_options = st.sidebar.selectbox(
+        'Select the work setup to be displayed',
+        ('Work from Home', 'Work from Office', 'Both'))
 
-    return fatigue_score, gender_options, designation, company_options
+    return fatigue_score, gender_options, designation, company_options, wfh_options

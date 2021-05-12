40cb39949a34248622c701e97773caffcabec1c4
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-05-12 10:10:47 -0700

    setup sphinx and refactor code

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-
+sys.path.insert(0, os.path.abspath(os.path.join('..')))
 
 # -- Project information -----------------------------------------------------
 
-project = 'ECE 229 Group 7 Insights into Mental Fatigue at Work'
+project = 'ECE 229 Group 7'
 copyright = '2021, Cyrus Shen, Fengqi Yang, Jessica Echterhoff, Qiyao Wu, Ravi Patel, Zihao Mo'
 author = 'Cyrus Shen, Fengqi Yang, Jessica Echterhoff, Qiyao Wu, Ravi Patel, Zihao Mo'
 
@@ -30,8 +29,7 @@ release = 'June 2021'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = [ 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,4 +50,5 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
+

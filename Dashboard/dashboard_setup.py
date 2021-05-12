f649b627006236c0866fc44f39e632851ca04cc8
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-05-11 20:22:28 -0700

    add side menu items to display data distribution

--- Dashboard/dashboard_setup.py
+++ Dashboard/dashboard_setup.py
@@ -9,87 +9,59 @@ import time
 import plotly.figure_factory as ff
 import matplotlib.pyplot as plt
 import plotly.express as px
+from sidebar import * 
 
-'''
-Setting title
-'''
-st.title("Understanding Employee's Stress Level")
-st.write("""
-## Welcome to the Dashboad :) 
-""")
-
-'''
-Table 1:
-'''
-st.write("Here's the data used to train prediction model")
-train = pd.read_csv('../data/train.csv')
-#remove missing values form last three coloms
-train = train.dropna(subset=['Resource Allocation','Mental Fatigue Score',
+def load_data():
+    train = pd.read_csv('../data/train.csv')
+    #remove missing values form last three coloms
+    train = train.dropna(subset=['Resource Allocation','Mental Fatigue Score',
                              'Burn Rate']).reset_index(drop=True)
-st.write(train)
+    return train 
 
 
+def main():
+    st.title("Understanding Employee's Stress Level")
+    st.write("""
+    ## Welcome to the Dashboad :) 
+    """)
+    train = load_data()
 
-'''
-Chart 1:
-'''
-#histogram
-st.write("Example of line chart")
-df = pd.DataFrame(train, columns = ['Mental Fatigue Score','Burn Rate'])
-df = df.rename(columns = {'Mental Fatigue Score': 'fatigue'}, inplace = False)
-values = st.sidebar.slider('Fatigure Score', 0., 10., (1., 9.))
-f = px.histogram(df.query(f'fatigue.between{values}',engine='python'), x='fatigue', nbins=15, title='Fatigue Score Distribution')
-f.update_xaxes(title='Mental Fatigue Score')
-f.update_yaxes(title='Num of People')
-st.plotly_chart(f)
+    st.write("Here's the data used to train prediction model")
 
+    st.write(train)
 
+    df = pd.DataFrame(train, columns = ['Mental Fatigue Score','Burn Rate'])
+    df = df.rename(columns = {'Mental Fatigue Score': 'fatigue'}, inplace = False)
 
-
-
-st.write("Example of plotting a map")
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [32.88, -117.20],
-    columns=['lat', 'lon']
-)
-st.map(map_data)
-
-st.write('Example of checkboxes')
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-        np.random.randn(20, 3),
-        columns=['a', 'b', 'c']
-    )
-    chart_data
+    fatigue_score, gender_options, designation, company_options = setup_sidebar()
 
 
-# st.write("Here's example that using data to create a table:")
-df2 = pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-})
-# df
-st.write('Example of using a selectbox for options')
-option = st.selectbox(
-    'Which number do you like best?',
-    df2['first column']
-)
-'You selected: ', option
+    average_male = train.query('Gender == \'Male\'',engine='python').mean()
+    average_female = train.query('Gender == \'Female\'',engine='python').mean()
 
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button('Press')
-if pressed:
-    right_column.write('You press it!')
+    fig = px.bar([0,1], [average_male[2], average_female[2]])
+    fig.update_xaxes(title='Average Mental Fatigue Score')
+    fig.update_yaxes(title='Gender')
+    st.plotly_chart(fig)
 
-expander = st.beta_expander("FAQ")
-expander.write("Here is the FAQ section")
+    setup_distribution_plot(train, fatigue_score, gender_options, designation, company_options)
 
-st.write("Example of showing progress")
-'Starting a long computation...'
-latest_iteration = st.empty()
-bar = st.progress(0)
-for i in range(100):
-    latest_iteration.text(f'Iteration{i + 1}')
-    bar.progress(i + 1)
-    time.sleep(0.1)
-'...and now we are done!'
+def setup_distribution_plot(train, fatigue_score, gender_options, designation, company_options):
+    '''sets up a plot that is controlled with the sidepanels to control the distribution of the data in a histogram'''
+    train_new = train
+    train_new['fatigue'] = train_new["Mental Fatigue Score"]
+    train_new['company'] = train_new["Company Type"]
+
+    plot_query = f'fatigue.between{fatigue_score} and Designation.between{designation}'
+    if gender_options != "Both":
+        plot_query = plot_query + f' and Gender == \'{gender_options}\''
+    if company_options != "Both":
+        plot_query = plot_query + f' and company == \'{company_options}\''
+
+    f = px.histogram(train_new.query(plot_query,engine='python'), x='Mental Fatigue Score', nbins=15, title='Fatigue Score Distribution')
+    f.update_xaxes(title='Mental Fatigue Score')
+    f.update_yaxes(title='Gender')
+    st.plotly_chart(f)
+
+if __name__ == "__main__":
+    main()

2be0ce53d4388e46a15d5df5e75667483bb9bfc9
nicoleyfq <fey002@ucsd.edu>
2021-05-18 18:00:16 -0700

    added visualization to dashboard

--- Dashboard/dashboard_setup.py
+++ Dashboard/dashboard_setup.py
@@ -2,6 +2,7 @@
 This file intend to build a dashboard using streamlit to
 provide end user visualization and analysis.
 '''
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -20,18 +21,52 @@ def load_data():
                              'Burn Rate']).reset_index(drop=True)
     return train 
 
+def make_header():
+    '''setup header section and allow user to select/input data file for analysis'''
+    row0_spacer1, row0_1, row0_spacer2, row0_2, row0_spacer3 = st.beta_columns(
+    (.1, 2, .2, 1, .1))
+
+    row0_1.title("Understanding Your Employee's Stress Level")
+
+    with row0_2:
+        st.write('')
+
+    row0_2.subheader('A Web App by [Winter 2021 ECE 229 Group 7](https://github.com/rpatel26/ECE229Group7)')
+    
+    row1_spacer1, row1_1, row1_spacer2 = st.beta_columns((.1, 3.2, .1))
+
+    with row1_1:
+        st.markdown("Welcome to Employee Stress Analysis App. Our project team aims to predict, analyze and visualize how stressed employees in a company are, by looking at various metrics such as how long employees have been in the company, their work type, working hours, setups, benefits, etc. Give it a go!")
+        st.markdown("**To begin, please upload your company's survey data (or just use our sample data!).** 👇")
+
+    row2_spacer1, row2_1, row2_spacer2 = st.beta_columns((.1, 3.2, .1))
+    with row2_1:
+        default_username = st.selectbox("Select one of our sample data", ("Employee-Data-Sample-1","Employee-Data-Sample-2"))
+        st.markdown("**or**")
+        user_input = st.text_input(
+            "Input your own employee data file(csv) path here (e.g. https://github.com/rpatel26/ECE229Group7)")
+        need_help = st.beta_expander('Need help? 👉')
+        with need_help:
+            st.markdown("Having trouble loading your data file? Read the data fields template here https://github.com/rpatel26/ECE229Group7.")
+
+        if not user_input:
+            df = load_data()
+        else:
+            try:
+                with open(user_input) as input:
+                    st.text(input.read())
+            except FileNotFoundError:
+                st.error('File not found.')
+        
+        st.write(df)
+        return df
+
 def main():
     '''main function to set up the streamlit application visuals'''
-    st.title("Understanding Employee's Stress Level")
-    st.write("""
-    ## Welcome to the Dashboad :) 
-    """)
-    train = load_data()
-
-    st.write("Here's the data used to train prediction model")
-
-    st.write(train)
-
+    st.set_page_config(layout="wide")
+    train = make_header()
+    data_summary(train)  
+    features_plot(train)
     df = pd.DataFrame(train, columns = ['Mental Fatigue Score','Burn Rate'])
     df = df.rename(columns = {'Mental Fatigue Score': 'fatigue'}, inplace = False)
 
@@ -40,7 +75,7 @@ def main():
     setup_male_female_avg_plot(train)
     setup_distribution_plot(train, fatigue_score, gender_options, designation, company_options, wfh_options)
     
-    st.write("## Using model to predict your burnout score")
+    st.title("Using model to predict burn rate")
     burnout_score = get_user_input()
     st.write("Your predicted burnout score is (scale 0-1):",burnout_score)
 
